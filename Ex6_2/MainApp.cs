6da909634c6882cc4106202cea7f83832703d3c3
Nicholas H. Park <Nicholas H. Park@NICK-WIN>
2020-12-10 14:57:08 +0900

    연습문제

---
+++ Ex6_2/MainApp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex6_2
+{
+    class MainApp
+    {
+        public static void Main()
+        {
+            double mean = 0;
+
+            Mean(1, 2, 3, 4, 5, ref mean);
+
+            Console.WriteLine("평균 : {0}", mean);
+        }
+
+        public static void Mean(
+            double a, double b, double c,
+            double d, double e, ref double mean)
+        {
+            mean = (a + b + c + d + e) / 5;
+        }
+    }
+}
