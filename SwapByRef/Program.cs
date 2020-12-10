7bfef2b29d77fc6e479d4fa6806306678fdf80ea
Nicholas H. Park <Nicholas H. Park@NICK-WIN>
2020-12-10 13:50:53 +0900

    1st Solution

---
+++ SwapByRef/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapByRef
+{
+    class MainApp
+    {
+        static void Swap(ref int a, ref int b)
+        {
+            int temp = b;
+            b = a;
+            a = temp;
+        }
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int y = 4;
+
+            Console.WriteLine($"Before Swap x:{x}, y:{y}");
+
+            Swap(ref x, ref y);
+
+            Console.WriteLine($"After Swap x:{x}, y:{y}");
+        }
+    }
+}
