b46bcabe1658c01e373b2827fa3c0e12bcb63baa
Nicholas H. Park <Nicholas H. Park@NICK-WIN>
2020-12-10 14:30:17 +0900

    NamedParameter

---
+++ NamedParameter/MainApp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamedParameter
+{
+    class MainApp
+    {
+        static void PrintProfile(string name, string phone)
+        {
+            Console.WriteLine($"Name:{name}, Phone:{phone}");
+        }
+        static void Main(string[] args)
+        {
+            PrintProfile(name: "박찬호", phone: "010-123-1234");
+            PrintProfile(phone: "010-987-9877", name: "박지성");
+            PrintProfile("박세리", "010-111-2222");
+            PrintProfile("박상현", phone: "010-123-1112");
+        }
+    }
+}
